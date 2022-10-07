626875bfb4cda1f1bd5709950cceee090401ff81
Jannat Rashid <97726066+jannatRashid@users.noreply.github.com>
2022-10-07 06:11:14 -0700

    Update columnmajor.cpp

--- columnmajor.cpp
+++ columnmajor.cpp
@@ -1,47 +1,51 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-    int row, column;
-    cout << "enter number of rows = ";
-    cin >> row;
-    cout << "enter number of columns = ";
-    cin >> column;
-    int list[row][column];
-    for (int j = 0; j < column; j++)
-    {
-        for (int i = 0; i < row; i++)
-        {
-            cout << "enter elements ";
-            cin >> list[i][j];
+int main(){
+     int row,column;
+    cout<<"enter number of rows = ";
+    cin>>row;
+    cout<<"enter number of columns = ";
+    cin>>column;
+    int list[column][row];
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++){
+            cout<<"enter elements ";
+            cin>>list[i][j];
         }
-    }
-    for (int j = 0; j < column; j++)
-    {
-        for (int i = 0; i < row; i++)
-        {
-            printf("%u", list[i][j]);
+}
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++){
+            printf("%u",list[i][j]);
         }
-        cout << "\n";
+        cout<<"\n";
     }
-    int *ba;
-    ba = &list[0][0];
-    cout << "base address" << endl;
+   int *ba ;
+    ba=&list[0][0];
+cout<<"base address"<<endl;
 
-    printf("%u", &ba);
+    printf("%u",&ba);
+// cout<<"base address"<<endl;
+    // printf("%u",&list[0][0]);
+    cout<<endl;
+     int x;
+     int y;
+    cout<<"enter coordinate at x = "<<endl;
+    cin>>x;
+    cout<<"enter coordinate at y = "<<endl;
+    cin>>y;
+   printf("%u",8125864+((y*column+x)*4));
+   int u;
+   int v;
+   cout<<endl;
+//    cout<<"enter coordinates to get the index number :"<<endl;
+//    cin>>u;
+//    cin>>v;
+   cout<<"index  number ::";
+   printf("%u",(y*column+x));
+    cout<<endl;
+   cout<<"value at index is :";
+   
+   cout<<endl;
+   cout<<list[x][y];
 
-    cout << endl;
-    int x;
-    int y;
-    cout << "enter coordinate at x = " << endl;
-    cin >> x;
-    cout << "enter coordinate at y = " << endl;
-    cin >> y;
-    printf("%u", 8125864 + ((y * column + x) * 4));
-    int u;
-    int v;
-    cout << endl;
-
-    cout << "index  number ::";
-    printf("%u", (y * column + x));
 }
